31a24415eb846901edf22307572f9ab88d175727
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-02 14:56:47 +0900

    AnimatePresence를 통한 컴포넌트가 사라질때 animate 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import React, { useEffect, useState } from "react";
 
 const Wrapper = styled(motion.div)`
@@ -24,21 +19,20 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 1 0px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Svg = styled.svg`
-  width: 300px;
-  height: 300px;
-  color: transparent;
-  path {
-    stroke: white;
-    stroke-width: "2";
-  }
-`;
-
-const svgVariants = {
-  start: { pathLength: 0, fill: "rgba(255,255,255,0)" },
-  end: {
-    pathLength: 1,
-    fill: "rgba(255,255,255,1)",
+const boxVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    scale: 0,
+    y: 20,
   },
 };
 
@@ -50,7 +44,16 @@ function App() {
   return (
     <Wrapper>
       <button onClick={onClick}>Click</button>
-      {showing ? <Box></Box> : null}
+      <AnimatePresence>
+        {showing ? (
+          <Box
+            variants={boxVariants}
+            initial="initial"
+            animate="visible"
+            exit="leaving"
+          ></Box>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
