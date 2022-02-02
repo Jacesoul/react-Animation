5bbf272460a44dc492fed86502aafed068b61872
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-02 12:26:36 +0900

    Framer Demo : Drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,10 +10,20 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: #d6bebe;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 1 0px 20px rgba(0, 0, 0, 0.06);
 `;
@@ -23,21 +34,27 @@ const boxVariants = {
   drag: {
     backgroundColor: "rgb(46,204,113)",
     transition: {
-      duration: 10,
+      duration: 3,
     },
   },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        whileDrag="drag"
-        variants={boxVariants}
-        whileHover="hover"
-        whileTap="click"
-      ></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0}
+          dragConstraints={biggerBoxRef}
+          whileDrag="drag"
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
